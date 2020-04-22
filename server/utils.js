f032936a2f6c5ad4e2fff232502762a99e78a5af
Christopher Sovine <chewey88@gmail.com>
2020-04-22 14:52:54 -0600

    Tests-aftn-proj

--- server/utils.js
+++ server/utils.js
@@ -1,16 +1,20 @@
 module.exports = {
   wordCount(posts) {
-    return posts.reduce(
+    return posts
+    .reduce(
       (accum, post) => (accum += post.text.split(' ').length),
       0,
     );
   },
   attachUserName(users, posts) {
-    let userDict = users.reduce((accum, user) => {
+    let userDict = users
+    .reduce((accum, user) => {
       accum[user.id] = user;
       return accum;
     }, {});
-    return posts.map(post => {
+    return posts
+    .filter(post => userDict[post.userId])
+    .map(post => {
       post.displayName = `${userDict[post.userId].first} ${
         userDict[post.userId].last
       }`;
